80374ffbd24fe7e8fec5d9410b83ba052cda1a25
SanyaNya <sanyanya4ever@gmail.com>
2024-07-21 16:59:35 +0300

    Allow overwrite facs

--- script.py
+++ script.py
@@ -149,7 +149,7 @@ def run_asr(tsk,lst):
             os.rename(f"{newdir}\\Shader", f"{newdir}\\.Shader")
         except Exception:
             pass
-        shutil.copytree("FACS_Utilities", f"{newdir}\\FACS_Utilities")
+        shutil.copytree("FACS_Utilities", f"{newdir}\\FACS_Utilities", dirs_exist_ok=True)
 
         while lock.locked():
             pass #wait to unlock lock by other thread
