8fc4c384e51bcfc64febd5bb8e660fe219cb4325
SanyaNya <sanyanya4ever@gmail.com>
2024-07-21 17:02:33 +0300

    Update or install script

--- update_or_install.bat
+++ update_or_install.bat
@@ -1,5 +1,6 @@
 
 powershell -Command "Invoke-WebRequest https://github.com/SanyaNya/AssetRipper/releases/download/1.0.18-r23/AssetRipper-Console-win-x64-1.0.18-r23.zip -OutFile package.zip"
+rmdir /Q /S AssetRipper
 mkdir AssetRipper
 cd AssetRipper
 tar -xvf ..\package.zip
@@ -7,12 +8,14 @@ cd ..
 del package.zip
 
 powershell -Command "Invoke-WebRequest https://github.com/FACS01-01/FACS_Utilities/releases/download/24.05.03/FACS.Utilities.24.05.03.zip -OutFile package.zip"
+rmdir /Q /S FACS_Utilities
 mkdir FACS_Utilities
 cd FACS_Utilities
 tar -xvf ..\package.zip
 cd ..
 del package.zip
 
+rmdir /Q /S vrcrip
 python -m venv vrcrip
 call ./vrcrip/Scripts/activate.bat
 pip install vrchatapi
